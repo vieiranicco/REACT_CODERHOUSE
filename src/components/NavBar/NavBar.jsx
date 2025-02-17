ec9f2e1302cf3ee16f2a0f65071ae6332d9492a3
vieiranicco <nicollas.contato@hotmail.com>
2025-02-16 22:11:08 -0300

    ex_aula_03

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,43 +1,41 @@
 import "./style.css"
+import "bootstrap/dist/css/bootstrap.min.css"
 
-export function NavBar() {
+export const NavBar = () => {
 	return (
-		<nav style={styles.navbar}>
-			<h1 style={styles.brand}>
-				<a href="#">SHOeCOMMERCE</a>
-			</h1>
-			<ul style={styles.menu}>
-				<li>
-					<a href="#">Início</a>
-				</li>
-				<li>
-					<a href="#">Produtos</a>
-				</li>
-				<li>
-					<a href="#">Contato</a>
-				</li>
-			</ul>
+		<nav className="navbar navbar-expand-lg">
+			<div className="container-fluid">
+				<a className="navbar-brand" href="#">
+					SHOeCOMMERCE
+				</a>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarNav"
+				>
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div className="collapse navbar-collapse" id="navbarNav">
+					<ul className="navbar-nav">
+						<li className="nav-item">
+							<a className="nav-link active" href="#">
+								Home
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">
+								Produtos
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">
+								Contato
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
 		</nav>
 	)
-}
-
-const styles = {
-	navbar: {
-		display: "flex",
-		justifyContent: "space-between",
-		alignItems: "center",
-		padding: "15px 20px",
-		backgroundColor: "#ff94c6",
-		color: "#000"
-	},
-	brand: {
-		margin: 0
-	},
-	menu: {
-		listStyle: "none",
-		display: "flex",
-		gap: "15px",
-		padding: 0,
-		margin: 0
-	}
 }
